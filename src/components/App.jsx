29404bc007dd9dad0b98aa18cb74e1caa6eca37c
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-15 22:57:11 +0200

    adds delete button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,46 @@
 import React, { useState } from 'react';
+import shortid from 'shortid';
 import ContactsForm from './ContactsForm/ContactsForm';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
 
-
-import css from './App.module.css';
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
   const handleAddContact = (name, number) => {
-    if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
-
     const newContact = {
-      id: Date.now().toString(),
+      id: shortid.generate(),
       name,
       number,
     };
 
-    setContacts(prevContacts => [...prevContacts, newContact]);
+    setContacts([...contacts, newContact]);
   };
 
-  const handleFilterChange = event => {
+  const handleDeleteContact = (id) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(updatedContacts);
+  };
+
+  const handleFilterChange = (event) => {
     setFilter(event.target.value);
   };
 
-  const filteredContacts = contacts.filter(contact =>
+  const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
-    <div className={css.container}>
+    <div>
       <h1>Phonebook</h1>
       <ContactsForm onAddContact={handleAddContact} />
       <h2>Contacts</h2>
       <Filter value={filter} onChange={handleFilterChange} />
-      <ContactsList contacts={filteredContacts} />
+      <ContactsList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </div>
   );
 };
 
 export default App;
-
--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -6,8 +6,11 @@ const ContactsForm = ({ onAddContact }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
     onAddContact(name, number);
     setName('');
     setNumber('');
@@ -15,26 +18,17 @@ const ContactsForm = ({ onAddContact }) => {
 
   return (
     <form className={css.contactsForm} onSubmit={handleSubmit}>
-
       <input
         type="text"
-        name="name"
-        placeholder="Enter name"
+        placeholder="Name"
         value={name}
-        onChange={event => setName(event.target.value)}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash, and spaces."
-        required
+        onChange={(event) => setName(event.target.value)}
       />
       <input
-        type="tel"
-        name="number"
-        placeholder="Enter phone number"
+        type="text"
+        placeholder="Number"
         value={number}
-        onChange={event => setNumber(event.target.value)}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses, and start with +."
-        required
+        onChange={(event) => setNumber(event.target.value)}
       />
       <button type="submit">Add contact</button>
     </form>
