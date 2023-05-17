719dd10a8a3fbb66603e9f913acd53b589c3f79d
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-17 12:38:32 +0200

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -45,62 +45,115 @@
 
 // export default ContactsForm;
 
-import React from 'react';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import css from './ContactsForm.module.css';
+
+// class ContactsForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: '',
+//       number: '',
+//     };
+//   }
+
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+//     const { name, number } = this.state;
+//     if (name.trim() === '' || number.trim() === '') {
+//       return;
+//     }
+//     const formattedName = name.trim().toLowerCase();
+//     this.props.onAddContact(formattedName, number);
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   handleChange = (event) => {
+//     const { name, value } = event.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+//       <form className={css.contactsForm} onSubmit={this.handleSubmit}>
+//         <label>Name</label>
+//         <input
+//           type="text"
+//           name="name"
+//           placeholder="Enter name"
+//           value={name}
+//           onChange={this.handleChange}
+//         />
+//         <label>Phone Number</label>
+//         <input
+//           type="text"
+//           placeholder="Phone Number"
+//           value={number}
+//           onChange={this.handleChange}
+//         />
+//         <button type="submit">Add contact</button>
+//       </form>
+//     );
+//   }
+// }
+
+// ContactsForm.propTypes = {
+//   onAddContact: PropTypes.func.isRequired,
+// };
+
+// export default ContactsForm;
+// ContactsForm.jsx
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactsForm.module.css';
 
-class ContactsForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      number: '',
-    };
-  }
+const ContactsForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { name, number } = this.state;
     if (name.trim() === '' || number.trim() === '') {
       return;
     }
-    const formattedName = name.trim().toLowerCase();
-    this.props.onAddContact(formattedName, number);
-    this.setState({ name: '', number: '' });
+    const formattedName = capitalizeFirstLetter(name.trim()); // Capitalize the first letter of the name
+    onAddContact(formattedName, number);
+    setName('');
+    setNumber('');
   };
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={css.contactsForm} onSubmit={this.handleSubmit}>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter name"
-          value={name}
-          onChange={this.handleChange}
-        />
-        <label>Phone Number</label>
-        <input
-          type="text"
-          placeholder="Phone Number"
-          value={number}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={css.contactsForm} onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        placeholder="Enter name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <label>Phone Number</label>
+      <input
+        type="text"
+        name="number"
+        placeholder="Phone Number"
+        value={number}
+        onChange={(event) => setNumber(event.target.value)}
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
 
 ContactsForm.propTypes = {
   onAddContact: PropTypes.func.isRequired,
 };
 
 export default ContactsForm;
+
